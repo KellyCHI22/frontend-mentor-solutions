a7acaa8eddade885862a66f15e231292f07346c2
Hsin-tingCHI <chihsinting@gmail.com>
2022-10-20 20:42:23 +0200

    Update js & css

--- 09-faq-accordion-card/app.js
+++ 09-faq-accordion-card/app.js
@@ -1,24 +1,31 @@
 const questions = document.querySelectorAll(".faq__question");
-const box = document.querySelector(".img-box");
-let boxPosition = 190
 
 questions.forEach(question => {
-    let answer = question.nextElementSibling;
-    let arrowIcon = question.firstElementChild;
-    question.addEventListener('click', e => {
-        console.log(answer)
-        if (!answer.style.display || answer.style.display === "none") {
-            answer.style.display = "block";
-            arrowIcon.style.transform = "rotate(180deg)";
-            question.style.fontWeight = "700";
-            boxPosition -= 5
-            box.style.top = `${boxPosition}px`
+    question.addEventListener("click", function () {
+        let answer = question.nextElementSibling;
+        if (answer.style.display === "block") {
+            hideAnswer(question)
         } else {
-            answer.style.display = "none";
-            arrowIcon.style.transform = "rotate(0deg)";
-            question.style.fontWeight = "400";
-            boxPosition += 5
-            box.style.top = `${boxPosition}px`
+            questions.forEach(question => {
+                hideAnswer(question)
+            })
+            displayAnswer(question)
         }
     })
 })
+
+function displayAnswer(target) {
+    let answer = target.nextElementSibling;
+    let arrowIcon = target.firstElementChild;
+    answer.style.display = "block";
+    arrowIcon.style.transform = "rotate(180deg)";
+    target.style.fontWeight = "700";
+}
+
+function hideAnswer(target) {
+    let answer = target.nextElementSibling;
+    let arrowIcon = target.firstElementChild;
+    answer.style.display = "none";
+    arrowIcon.style.transform = "rotate(0deg)";
+    target.style.fontWeight = "400";
+}
