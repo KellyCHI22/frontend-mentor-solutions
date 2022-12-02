c31671e538071115c2f01cb68cd6292fe9628515
Hsin-tingCHI <chihsinting@gmail.com>
2022-12-02 13:46:09 +0100

    Add challenge#17

---
+++ 17-time-tracking-dashboard/README.md
@@ -0,0 +1,66 @@
+# Frontend Mentor - Time tracking dashboard solution
+
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Switch between viewing Daily, Weekly, and Monthly stats
+
+### Screenshots
+#### Mobile version
+![](./screenshot_1.jpg)
+#### Desktop version - Daily mode
+![](./screenshot_2.jpg)
+#### Desktop version - Monthly mode
+![](./screenshot_3.jpg)
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- CSS Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Vanilla JavaScript
+- Local JSON file
+
+### What I learned
+
+I encountered some asynchronous problems when using fetch API to get my local JSON data. If I don't use `async` and `await`, timeData will not be able access the data from `data.json`. Another to note is that I should always `return` the promise, otherwise it's not going to work! 
+
+⚠️ Remember to return the promise when using `fetch()`!
+
+```js
+const timeData = [];
+
+async function getJSON() {
+    return fetch('./data.json')
+        .then(response => response.json())
+        .then(data => timeData.push(...data))
+        .catch(error => console.log(error));
+}
+
+await getJSON();
+renderDashboard();
+```
+
+## Author
+
+- Website - [My Github homepage](https://github.com/KellyCHI22)
+- Frontend Mentor - [@Hsin-tingCHI](https://www.frontendmentor.io/profile/Hsin-tingCHI)
+
+## Acknowledgments
+
+Thank you Frontend Mentor for providing the challenge!
---
+++ 17-time-tracking-dashboard/app.js
@@ -0,0 +1,103 @@
+// get DOM element variables
+const dashboard = document.getElementById('dashboard');
+const timeData = [];
+let currentTimeFrame = 'daily';
+
+// execution
+await getJSON();
+renderDashboard();
+dashboard.addEventListener('click', changeTimeFrame);
+
+// functions
+async function getJSON() {
+    return fetch('./data.json')
+        .then(response => response.json())
+        .then(data => timeData.push(...data))
+        .catch(error => console.log(error));
+}
+
+function renderDashboard() {
+    dashboard.innerHTML = '';
+    dashboard.innerHTML += generateUserInfo();
+    timeData.forEach(data => {
+        dashboard.innerHTML += generateTimeDashboard(data, currentTimeFrame);
+    });
+    setActiveTimeFrame();
+}
+
+function changeTimeFrame(e) {
+    if (!e.target.dataset.range) return;
+
+    if (e.target.dataset.range === 'daily') {
+        currentTimeFrame = 'daily';
+        renderDashboard();
+    } else if (e.target.dataset.range === 'weekly') {
+        currentTimeFrame = 'weekly';
+        renderDashboard();
+    } else if (e.target.dataset.range === 'monthly') {
+        currentTimeFrame = 'monthly';
+        renderDashboard();
+    }
+}
+
+function setActiveTimeFrame() {
+    const timeFrames = document.querySelectorAll('[data-range]');
+    timeFrames.forEach(timeFrame => {
+        if (timeFrame.dataset.range === currentTimeFrame) {
+            timeFrame.classList.add('range--selected');
+        } else {
+            timeFrame.classList.remove('range--selected');
+        }
+    });
+}
+
+function generateUserInfo() {
+    return `
+    <div class="user">
+        <div class="user__info">
+        <img src="images/image-jeremy.png" alt="" class="user__img">
+        <div>
+            <p>Report for</p>
+            <h2 class="user__name">Jeremy Robson</h2>
+        </div>
+        </div>
+        <div class="user__time-range">
+        <p class="" data-range="daily">Daily</p>
+        <p class="" data-range="weekly">Weekly</p>
+        <p class="" data-range="monthly">Monthly</p>
+        </div>
+    </div>
+    `;
+};
+
+function generateTimeDashboard(data, timeFrame) {
+    let range;
+    let before;
+
+    if (currentTimeFrame === 'daily') {
+        range = data.timeframes.daily;
+        before = 'Yesterday';
+    } else if (currentTimeFrame === 'weekly') {
+        range = data.timeframes.weekly;
+        before = 'Last week';
+    } else if (currentTimeFrame === 'monthly') {
+        range = data.timeframes.monthly;
+        before = 'Last month';
+    }
+
+    return `
+    <div class="dashboard-item ${data.title.toLowerCase()}">
+        <img src="images/icon-${data.title}.svg" aria-hidden="true" alt="" class="bg-icon">
+        <div class="dashboard-item__content">
+            <div class="d-flex">
+                <h3 class="dashboard-item__title">${data.title}</h3>
+                <a href="#" class="item__link"><img src="images/icon-ellipsis.svg" alt=""></a>
+            </div>
+            <div class="d-flex item__time">
+                <h2 class="dashboard-item__time">${range.current}hrs</h2>
+                <span class="dashboard-item__lastweek">${before} - ${range.previous}hrs</span>
+            </div>
+        </div>
+    </div> 
+    `;
+}
